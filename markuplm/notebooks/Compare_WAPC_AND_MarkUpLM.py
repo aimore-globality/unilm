93a95c1ab610a78c1afcf78cdaca89642e1a2147
aimore <aimore.dutra@globality.com>
2022-03-17 12:02:28 +0000

    Added new metric computation and plots

--- markuplm/notebooks/Compare_WAPC_AND_MarkUpLM.py
+++ markuplm/notebooks/Compare_WAPC_AND_MarkUpLM.py
@@ -176,6 +176,233 @@ df_domain_metrics = df.groupby("domain").apply(lambda x: compute_segmentation_me
 df_domain_metrics.mean().to_dict()
 # -
 
+df_domain_metrics
+
+# +
+debug = False
+negative_percentage = 0.10
+
+def compute_f1(precision, recall):
+    if precision and recall:
+        return 2*(precision * recall) / (precision + recall)
+    else:
+        return None
+
+def compute_precision(TP, FP):
+    if TP + FP > 0:        
+        return TP/(TP + FP)
+    else:
+        return None
+
+def compute_recall(TP, FN):
+    if TP + FN > 0:        
+        return TP/(TP + FN)
+    else:
+        return None
+
+def compute_cf_matrix(ground_truth, prediction):
+    assert len(ground_truth) == len(prediction)
+    positive_index = ground_truth[ground_truth.apply(len) > 0].index
+    pos_ground_truth = ground_truth.loc[positive_index]
+    pos_prediction = prediction.loc[positive_index]
+
+    neg_ground_truth = ground_truth.loc[~ground_truth.isin(pos_ground_truth)]
+    neg_prediction = prediction.loc[~prediction.isin(pos_ground_truth)]
+
+    fp_scalar = 1/negative_percentage
+
+    prediction_set = {past_client for past_clients in prediction.values for past_client in past_clients}
+    ground_truth_set = {past_client for past_clients in ground_truth.values for past_client in past_clients}
+
+    pos_prediction_set = {past_client for past_clients in pos_prediction.values for past_client in past_clients}
+    pos_ground_truth_set = {past_client for past_clients in pos_ground_truth.values for past_client in past_clients}
+    
+    neg_prediction_set = {past_client for past_clients in neg_prediction.values for past_client in past_clients}
+    neg_ground_truth_set = {past_client for past_clients in neg_ground_truth.values for past_client in past_clients}
+
+    TP = len(prediction_set & ground_truth_set)
+    
+    FP = len(prediction_set - ground_truth_set)
+
+    FN = len(ground_truth_set - prediction_set)
+
+    FP_pos = len(pos_prediction_set - pos_ground_truth_set)
+    FP_neg = len(neg_prediction_set - neg_ground_truth_set) 
+    FP_adjusted = FP_pos + FP_neg * fp_scalar
+
+    precision = compute_precision(TP, FP)
+    precision_adjusted = compute_precision(TP, FP_adjusted)
+    recall = compute_recall(TP, FN)
+    f1 = compute_f1(TP, FN)
+    num_positives = TP + FN
+    
+    results = dict(
+        TP=TP, 
+        FP=FP, 
+        FN=FN,
+        num_positives=num_positives,
+        FP_adjusted=FP_adjusted,
+        precision=precision,
+        precision_adjusted=precision_adjusted,
+        recall=recall,
+        f1=f1,
+        )
+
+    return results
+
+cf_matrix = dict()
+for domain_name, domain_df in df.groupby("domain"):
+    cf_matrix[domain_name] = compute_cf_matrix(domain_df["ground_truth"], domain_df["predictions"])
+
+cf_matrix = pd.DataFrame(cf_matrix).T.sort_values("num_positives")
+len(cf_matrix)
+# -
+
+cf_matrix = cf_matrix[cf_matrix["num_positives"] > 0]
+cf_matrix
+
+# +
+import plotly.graph_objects as go
+
+
+def plot_performance(
+    domain,
+    TP,
+    FP,
+    FN,
+    num_positives,
+    precision,
+    recall,
+    f1,
+    title="Performance per Domain",
+):
+    trace_TP = go.Bar(
+        x=domain,
+        y=TP,
+        name="TP",
+        yaxis="y1",
+    )
+    trace_FP = go.Bar(
+        x=domain,
+        y=FP,
+        name="FP",
+        yaxis="y1",
+    )
+
+    trace_FN = go.Bar(
+        x=domain,
+        y=FN,
+        name="FN",
+        yaxis="y1",
+    )
+    trace_pos = go.Bar(
+        x=domain,
+        y=num_positives,
+        name="num_positives",
+        yaxis="y1",
+    )
+
+    trace_precision = go.Scatter(
+        x=domain,
+        y=100 * precision,
+        name="precision",
+        mode="lines+markers",
+        yaxis="y2",
+    )
+
+    trace_recall = go.Scatter(
+        x=domain,
+        y=100 * recall,
+        name="recall",
+        mode="lines+markers",
+        yaxis="y2",
+    )
+
+    data = [trace_TP, trace_FP, trace_FN, trace_pos, trace_precision, trace_recall]
+
+    layout = go.Layout(
+        title=title,
+        yaxis=dict(title="Counts", ),
+        yaxis2=dict(
+            title="%",
+            overlaying="y",
+            side="right",
+            range=[0, 100],
+        ),
+        width=len(domain) * 27 if len(domain) > 20 else 600,
+        height=600,
+        margin=dict(l=5, r=5, b=20, t=50, pad=1),
+        legend=dict(
+            x=0.007,
+            y=0.97,
+            bordercolor="Black",
+            borderwidth=1,
+        ),
+        
+    )
+    return go.Figure(data=data, layout=layout)
+
+
+
+# -
+
+plot_performance(
+    domain=cf_matrix.index,
+    TP=cf_matrix["TP"],
+    FP=cf_matrix["FP"],
+    FN=cf_matrix["FN"],
+    num_positives=cf_matrix["num_positives"],
+    precision=cf_matrix["precision"],
+    recall=cf_matrix["recall"],
+    f1=cf_matrix["f1"],
+        )
+
+plot_performance(
+    title="Performance per Domain Adjusted",
+    domain=cf_matrix.index,
+    TP=cf_matrix["TP"],
+    FP=cf_matrix["FP_adjusted"],
+    FN=cf_matrix["FN"],
+    num_positives=cf_matrix["num_positives"],
+    precision=cf_matrix["precision_adjusted"],
+    recall=cf_matrix["recall"],
+    f1=cf_matrix["f1"],
+        )
+
+cf_matrix
+
+
+# +
+def compute_metrics_for_dataset(cf_matrix):
+    cf_matrix_all = cf_matrix[["TP", "FP", "FN", "num_positives", "FP_adjusted"]].sum()
+    cf_matrix_all['precision'] = compute_precision(TP=cf_matrix_all['TP'], FP=cf_matrix_all['FP'])
+    cf_matrix_all['precision_adjusted'] = compute_precision(TP=cf_matrix_all['TP'], FP=cf_matrix_all['FP_adjusted'])
+    cf_matrix_all['recall'] = compute_recall(TP=cf_matrix_all['TP'], FN=cf_matrix_all['FN'])
+    cf_matrix_all['f1'] = compute_f1(precision=cf_matrix_all['precision'], recall=cf_matrix_all['recall'])
+    cf_matrix_all['domain'] = "All"
+    cf_matrix_all = pd.DataFrame(cf_matrix_all).T
+    cf_matrix_all = cf_matrix_all.set_index('domain').reset_index()
+    cf_matrix_all
+    return cf_matrix_all
+
+
+# -
+
+cf_matrix_all = compute_metrics_for_dataset(cf_matrix)
+cf_matrix_all
+
+plot_performance(
+    title="Performance per Domain Adjusted",
+    domain=cf_matrix_all.index,
+    TP=cf_matrix_all["TP"],
+    FP=cf_matrix_all["FP_adjusted"],
+    FN=cf_matrix_all["FN"],
+    num_positives=cf_matrix_all["num_positives"],
+    precision=cf_matrix_all["precision_adjusted"],
+    recall=cf_matrix_all["recall"],
+    f1=cf_matrix_all["f1"],
+        )
+
 # # Load MarkupLM Trained Model and predict on something 
 
 # ---
@@ -183,15 +410,15 @@ df_domain_metrics.mean().to_dict()
 # +
 import pandas as pd
 
-results = pd.read_pickle("results_classified.pkl")
+results = pd.read_pickle("results_classified_5_epoch.pkl")
 # -
 
 results
 
 results = results.reset_index().drop('index', axis=1)
-# pos_results = results[results['pred_type']=='PAST_CLIENT'] #! The Performance of the MarkupLM
+pos_results = results[results['pred_type']=='PAST_CLIENT'] #! The Performance of the MarkupLM
 # pos_results = results[results['truth']=='PAST_CLIENT'] #! The best this system can give, considering that the annotations for the text are correct. However, this might not be the best for the values.
-pos_results = results #! No classification 
+# pos_results = results #! No classification 
 len(pos_results)
 
 sementation_on_nodes  = pc.segmenter_html._segment_companies(pos_results['text']).fillna("").apply(list)
@@ -229,10 +456,11 @@ df = df.set_index("url")
 
 merge = df.join(results_grouped, lsuffix="_l")
 
+# #! Model in Production
 merge_domain_metrics = merge.groupby("domain").apply(lambda x: compute_segmentation_metrics(x.ground_truth, x.predictions, x.domain))
 merge_domain_metrics.mean().to_dict()
 
-# #! Positive Nodes are predictions by the model
+# #! Positive Nodes are predictions by the model trained on 5 epochs
 merge_domain_metrics = merge.groupby("domain").apply(lambda x: compute_segmentation_metrics(x.ground_truth, x.reconciliations_taxonomized, x.domain))
 merge_domain_metrics.mean().to_dict()
 
@@ -244,7 +472,17 @@ merge_domain_metrics.mean().to_dict()
 merge_domain_metrics = merge.groupby("domain").apply(lambda x: compute_segmentation_metrics(x.ground_truth, x.reconciliations_taxonomized, x.domain))
 merge_domain_metrics.mean().to_dict()
 
+merge_domain_metrics.dropna().sort_values("segmentation_f1").plot()
 
+merge_domain_metrics.to_csv("merge_domain_metrics_5_epochs.csv")
+
+merge_domain_metrics = pd.read_csv("merge_domain_metrics_5_epochs.csv")
+
+df_domain_metrics.to_csv("merge_domain_metrics_prod.csv")
+
+df_domain_metrics[['segmentation_f1', "segmentation_precision", "segmentation_recall"]].plot.bar(figsize=(30, 5))
+
+merge_domain_metrics[['segmentation_f1', "segmentation_precision", "segmentation_recall"]].plot.bar(figsize=(30, 5))
 
 # ---
 
