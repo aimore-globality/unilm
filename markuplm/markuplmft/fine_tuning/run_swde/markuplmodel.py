514b762f0b466fc2209208726ca5c7de6b41ec12
aimore <aimore.dutra@globality.com>
2022-04-05 10:46:29 +0000

    WIP

--- markuplm/markuplmft/fine_tuning/run_swde/markuplmodel.py
+++ markuplm/markuplmft/fine_tuning/run_swde/markuplmodel.py
@@ -1,7 +1,4 @@
 from __future__ import absolute_import, division, print_function
-import multiprocessing as mp
-import argparse
-from markuplmft.fine_tuning.run_swde.eval_utils import compute_metrics_per_dataset
 
 # import copy
 from pprint import pprint
@@ -15,24 +12,17 @@ from typing import List, Dict
 import pandas as pd
 import numpy as np
 import torch
-
 # from markuplmft.fine_tuning.run_swde.eval_utils import page_level_constraint
-from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
-from torch.utils.data.distributed import DistributedSampler
-from tqdm import tqdm, trange
-from transformers import WEIGHTS_NAME, AdamW, get_linear_schedule_with_warmup
-from markuplmft.fine_tuning.run_swde.utils import SwdeDataset, get_swde_features
 
 from markuplmft.fine_tuning.run_swde import constants
 from markuplmft.models.markuplm import (
     MarkupLMConfig,
     MarkupLMForTokenClassification,
     MarkupLMTokenizer,
+
 )
-from sklearn.metrics import confusion_matrix
 
 logger = logging.getLogger(__name__)
-
 
 def set_seed(n_gpu):
     """
@@ -47,72 +37,50 @@ def set_seed(n_gpu):
     if n_gpu > 0:
         torch.cuda.manual_seed_all(seed)
 
+seed = 66 #! Set the seed like other modules
 
-seed = 66  #! Set the seed like other modules
 
-
-class MarkupLModel:
-    def __init__(
-        self,
-        pre_trained_model_folder_path: str = "/data/GIT/unilm/markuplm/markuplmft/models/markuplm/286",
+class MarkupLModel():
+    def __init__(self, 
+    pre_trained_model_folder_path:str="/data/GIT/unilm/markuplm/markuplmft/models/markuplm/286",
+    verbose:bool=False,
     ):
         self.model = None
         self.tokenizer = None
-        self.no_cuda = False
-        self.local_rank = -1
 
         self.pre_trained_model_folder_path = pre_trained_model_folder_path
-
+        
         self.output_dir = "/data/GIT/unilm/markuplm/markuplmft/models/markuplm/"
-        self.overwrite_output_dir = True
-
+        
         self.model_name_or_path = "microsoft/markuplm-base"
         self.save_model_path = "/data/GIT/unilm/markuplm/markuplmft/models/markuplm/"
 
         self.doc_stride = 128
         self.max_seq_length = 384
         self.overwrite_cache = False
-        self.save_features = True
+        
+        if verbose:
+            print("self.__dict__", pprint(self.__dict__))
 
-        # ? Setup CUDA, GPU & distributed training
-        if self.local_rank == -1 or self.no_cuda:
-            self.device = torch.device(
-                "cuda" if torch.cuda.is_available() and not self.no_cuda else "cpu"
-            )
-            self.n_gpu = torch.cuda.device_count() if not self.no_cuda else 0
-        else:  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
-            torch.cuda.set_device(self.local_rank)
-            self.device = torch.device("cuda", self.local_rank)
-            torch.distributed.init_process_group(backend="nccl")
-            self.n_gpu = 1
 
-        print("self.__dict__", pprint(self.__dict__))
-
-    def load_pretrained_tokenizer():
+    def load_pretrained_tokenizer(self):
         self.tokenizer = MarkupLMTokenizer.from_pretrained(self.model_name_or_path)
 
-    def load_pretrained_model(self):
-
-        # Set seed
-        set_seed(self.n_gpu)
-
-        # Load pretrained model and tokenizer
-        if self.local_rank not in [-1, 0]:
+    def load_pretrained_model(self, local_rank):
+        #? Load pretrained model and tokenizer
+        if local_rank not in [-1, 0]:
             torch.distributed.barrier()
-            # Make sure only the first process in distributed training will download model & vocab
+            #? Make sure only the first process in distributed training will download model & vocab
 
         config = MarkupLMConfig.from_pretrained(self.pre_trained_model_folder_path)
         config_dict = config.to_dict()
         config_dict.update({"node_type_size": len(constants.ATTRIBUTES_PLUS_NONE)})
         config = MarkupLMConfig.from_dict(config_dict)
 
-
-        self.model = MarkupLMForTokenClassification.from_pretrained(
-            self.model_name_or_path, config=config
-        )
+        self.model = MarkupLMForTokenClassification.from_pretrained(self.model_name_or_path, config=config)
         self.model.resize_token_embeddings(len(self.tokenizer))
 
-        self.model.to(self.device)
+        # self.model.to(self.device) #! Move this to the training part at the correct place
 
         # self.tokenizer.save_pretrained(sub_output_dir)
 
@@ -128,138 +96,7 @@ class MarkupLModel:
         model_to_save = self.model.module if hasattr(self.model, "module") else self.model
         model_to_save.save_pretrained(self.save_model_path)
         self.tokenizer.save_pretrained(self.save_model_path)
-        #! Save the parameters: torch.save(self.__dict__, os.path.join(self.save_model_path, "training_args.bin"))
-
-    def predict_on_develop(self):
-        set_seed(self.n_gpu)
-        self.load_model()
-
-        dataset_predicted = pd.DataFrame()
-        for website in tqdm(self.websites):
-            website_predicted = self.predict_on_website(website)
-            website_predicted["domain"] = website
-            dataset_predicted = dataset_predicted.append(website_predicted)
-
-        return dataset_predicted
-
-    def predict_on_website(self, website):
-        dataset, info = self.get_dataset_and_info_for_websites([website], evaluate=True)
-
-        eval_batch_size = self.per_gpu_eval_batch_size * max(1, self.n_gpu)
-        # Note that DistributedSampler samples randomly
-        # In our setting, we should not apply DDP
-        eval_sampler = (
-            SequentialSampler(dataset) if self.local_rank == -1 else DistributedSampler(dataset)
-        )
-
-        eval_dataloader = DataLoader(dataset, sampler=eval_sampler, batch_size=eval_batch_size)
-
-        # multi-gpu evaluate
-        if self.n_gpu > 1 and not isinstance(self.model, torch.nn.DataParallel):
-            self.model = torch.nn.DataParallel(self.model)
-
-        # Eval!
-        logger.info(f"***** Running evaluation *****")
-        logger.info(f"  Num examples for {website} = {len(dataset)}")
-        logger.info(f"  Batch size = {eval_batch_size}")
-
-        all_logits = []
-        for batch in tqdm(eval_dataloader, desc="Evaluating"):
-            self.model.eval()
-            batch = tuple(
-                b.to(self.device) for b in batch
-            )  # TODO (AIMORE): Why they use tuple here?
-            with torch.no_grad():
-                inputs = {  # TODO (AIMORE): Can't this batch have better names, instead of these numbered indices?
-                    "input_ids": batch[0],
-                    "attention_mask": batch[1],
-                    "token_type_ids": batch[2],
-                    "xpath_tags_seq": batch[3],
-                    "xpath_subs_seq": batch[4],
-                }
-                outputs = self.model(**inputs)
-                logits = outputs["logits"]  # which is (bs,seq_len,node_type)
-                all_logits.append(logits.detach().cpu())
-
-        all_probs = torch.softmax(
-            torch.cat(all_logits, dim=0), dim=2
-        )  # (all_samples, seq_len, node_type)
-
-        assert len(all_probs) == len(info)
-
-        all_res = collections.defaultdict(dict)
-
-        for sub_prob, sub_info in zip(all_probs, info):
-            (
-                html_path,
-                involved_first_tokens_pos,
-                involved_first_tokens_xpaths,
-                involved_first_tokens_types,
-                involved_first_tokens_text,
-                involved_first_tokens_gt_text,
-            ) = sub_info
-
-            for pos, xpath, type, text, gt_text in zip(
-                involved_first_tokens_pos,
-                involved_first_tokens_xpaths,
-                involved_first_tokens_types,
-                involved_first_tokens_text,
-                involved_first_tokens_gt_text,
-
-            ):
-
-                pred = sub_prob[pos]  # ? This gets the first logit of each respective node
-                # ? sub_prob = [tensor([0.0045, 0.9955]), ...], sub_prob.shape = [384, 2]
-                # ? pos = 14
-                # ? pred = tensor([0.0045, 0.9955])
-                if xpath not in all_res[html_path]:
-                    all_res[html_path][xpath] = {}
-                    all_res[html_path][xpath]["pred"] = pred
-                    all_res[html_path][xpath]["truth"] = type
-                    all_res[html_path][xpath]["text"] = text
-                    all_res[html_path][xpath]["gt_text"] = gt_text
-
-                else:
-                    all_res[html_path][xpath]["pred"] += pred
-                    assert all_res[html_path][xpath]["truth"] == type
-                    assert all_res[html_path][xpath]["text"] == text
-
-        lines = {
-            "html_path": [],
-            "xpath": [],
-            "text": [],
-            "gt_text": [],
-            "truth": [],
-            "pred_type": [],
-            "final_probs": [],
-        }
-
-        for html_path in all_res:
-            # E.g. all_res [dict] = {html_path = {xpath = {'pred': tensor([0.4181, 0.5819]), 'truth': 'PAST_CLIENT', 'text': 'A healthcare client gains control of their ACA processes | BerryDunn'},...}, ...}
-            for xpath in all_res[html_path]:
-                final_probs = all_res[html_path][xpath]["pred"] / torch.sum(
-                    all_res[html_path][xpath]["pred"]
-                )  # TODO(aimore): Why is this even here? torch.sum(both prob) will always be 1, what is the point then? Maybe in case of more than one label?
-                pred_id = torch.argmax(final_probs).item()
-                pred_type = constants.ATTRIBUTES_PLUS_NONE[pred_id]
-                final_probs = final_probs.numpy().tolist()
-
-                lines["html_path"].append(html_path)
-                lines["xpath"].append(xpath)
-                lines["gt_text"].append(all_res[html_path][xpath]["gt_text"])
-                lines["text"].append(all_res[html_path][xpath]["text"])
-                lines["truth"].append(all_res[html_path][xpath]["truth"])
-                lines["pred_type"].append(pred_type)
-                lines["final_probs"].append(final_probs)
-
-        result_df = pd.DataFrame(lines)
-        return result_df
-
-    def evaluate(self, dataset_predicted):
-        metrics_per_dataset, cm_per_dataset = compute_metrics_per_dataset(dataset_predicted)
-        print(
-            f"Node Classification Metrics per Dataset: {metrics_per_dataset} | cm_per_dataset: {cm_per_dataset}"
-        )
+        #! Save the parameters: torch.save(self.__dict__, os.path.join(self.save_model_path, "training_args.bin"))        
 
     def save_model(self, sub_output_dir, global_step):
         output_dir = os.path.join(sub_output_dir, f"checkpoint-{global_step}")
@@ -274,7 +111,5 @@ class MarkupLModel:
     def load_model(self):
         config = MarkupLMConfig.from_pretrained(self.pre_trained_model_folder_path)
         self.tokenizer = MarkupLMTokenizer.from_pretrained(self.pre_trained_model_folder_path)
-        self.model = MarkupLMForTokenClassification.from_pretrained(
-            self.pre_trained_model_folder_path, config=config
-        )
+        self.model = MarkupLMForTokenClassification.from_pretrained(self.pre_trained_model_folder_path, config=config)
         self.model.to(self.device)
--- markuplm/markuplmft/fine_tuning/run_swde/trainer.py
+++ markuplm/markuplmft/fine_tuning/run_swde/trainer.py
@@ -1,7 +1,7 @@
 import json
 from logging import Logger
 from typing import Dict, List, Optional
-
+import os
 import numpy as np
 import pandas as pd
 import torch
@@ -43,67 +43,87 @@ def set_seed(n_gpu):
     if n_gpu > 0:
         torch.cuda.manual_seed_all(seed)
 
-seed = 66 #! Set the seed like other modules
+
+seed = 66  #! Set the seed like other modules
+
 
 class Trainer:
-
-
     def __init__(
         self,
         model,
-        no_cuda:bool,
+        no_cuda: bool,
         train_dataset,
         develop_dataset,
         per_gpu_train_batch_size: int,
         eval_batch_size: int,
         num_epochs: int,
+        max_steps: int,
+        logging_steps: int,
         weight_decay,
         learning_rate,
         adam_epsilon,
         warmup_ratio,
         verbose: bool,
         gradient_accumulation_steps: int,
-        output_dir:str,
-        fp16:bool=True,
-        fp16_opt_level: str="O1",
+        max_grad_norm: float,
+        output_dir: str,
+        fp16: bool = True,
+        fp16_opt_level: str = "O1",
+        overwrite_output_dir: bool = False,
     ):
         self.local_rank = -1
         self.fp16 = fp16
         self._setup_cuda_gpu(no_cuda)
         set_seed(self.n_gpu)
 
-        self.model = model
-        self.num_train_epochs = num_epochs
-
+        # ? Setting Data
         self.per_gpu_train_batch_size = per_gpu_train_batch_size
         self.train_batch_size = self.per_gpu_train_batch_size * max(1, self.n_gpu)
         self.eval_batch_size = eval_batch_size
 
         self.gradient_accumulation_steps = gradient_accumulation_steps
 
-        self.optimizer = self.prepare_optimizer(self.model, weight_decay, learning_rate, adam_epsilon)
+        self.train_dataset = train_dataset
+        self.develop_dataset = develop_dataset
+        self.train_dataloader = self._get_dataloader(self.train_dataset, True)
+        # self.develop_dataloader = self._get_dataloader(self.develop_dataset, False)
 
+        self.max_steps = max_steps
+        self.num_train_epochs = num_epochs
         if self.max_steps > 0:
             self.t_total = self.max_steps
             self.num_train_epochs = (
-                self.max_steps // (len(self.train_dataloader) // self.gradient_accumulation_steps) + 1
+                self.max_steps // (len(self.train_dataloader) // self.gradient_accumulation_steps)
+                + 1
             )
         else:
-            self.t_total = len(self.train_dataloader) // self.gradient_accumulation_steps * self.num_train_epochs
+            self.t_total = (
+                len(self.train_dataloader)
+                // self.gradient_accumulation_steps
+                * self.num_train_epochs
+            )
+
+        # ? Setting Model
+        self.model = model
+        self.model.to(self.device)
+        self.optimizer = self._prepare_optimizer(
+            self.model, weight_decay, learning_rate, adam_epsilon
+        )
 
         self.warmup_ratio = warmup_ratio
-        self.scheduler = self.prepare_scheduler(self.optimizer, self.warmup_ratio, self.t_total)
+        self.scheduler = self._prepare_scheduler(self.optimizer, self.warmup_ratio, self.t_total)
 
-
-        #? Set model to use fp16 and multi-gpu
+        # ? Set model to use fp16 and multi-gpu
         if self.fp16:
-            self.model, self.optimizer = amp.initialize(self.model, self.optimizer, opt_level=fp16_opt_level)
+            self.model, self.optimizer = amp.initialize(
+                self.model, self.optimizer, opt_level=fp16_opt_level
+            )
 
-        #? multi-gpu training (should be after apex fp16 initialization)
+        # ? multi-gpu training (should be after apex fp16 initialization)
         if self.n_gpu > 1 and not isinstance(self.model, torch.nn.DataParallel):
             self.model = torch.nn.DataParallel(self.model)
 
-        #? Distributed training (should be after apex fp16 initialization)
+        # ? Distributed training (should be after apex fp16 initialization)
         if self.local_rank != -1:
             self.model = torch.nn.parallel.DistributedDataParallel(
                 self.model,
@@ -112,7 +132,12 @@ class Trainer:
                 find_unused_parameters=True,
             )
 
+
+        self.max_grad_norm = max_grad_norm
+
         # self.logging_epoch = 0
+        self.evaluate_during_training = False
+        self.logging_steps = logging_steps  #! Maybe change this to logging_epoch
         self.save_steps = 1000
 
         self.verbose = verbose
@@ -120,22 +145,23 @@ class Trainer:
         # ? Check if the folder exists
         self.output_dir = output_dir
         if (
-                os.path.exists(self.output_dir)
-                and os.listdir(self.output_dir)
-                and not self.overwrite_output_dir
-            ):
-                raise ValueError(f"Output directory ({self.output_dir}) already exists and is not empty. Use --overwrite_output_dir to overcome.")
+            os.path.exists(self.output_dir)
+            and os.listdir(self.output_dir)
+            and not overwrite_output_dir
+        ):
+            raise ValueError(
+                f"Output directory ({self.output_dir}) already exists and is not empty. Use --overwrite_output_dir to overcome."
+            )
 
         if self.verbose:
             print("self.__dict__", pprint(self.__dict__))
 
-        self.train_dl = self._get_dataloader(train_dataset, True)
-        self.develop_dl = self._get_dataloader(develop_dataset, False)
-
     def _setup_cuda_gpu(self, no_cuda):
-        #? Setup CUDA, GPU & distributed training
+        # ? Setup CUDA, GPU & distributed training
         if self.local_rank == -1 or no_cuda:
-            self.device = torch.device("cuda" if torch.cuda.is_available() and not no_cuda else "cpu")
+            self.device = torch.device(
+                "cuda" if torch.cuda.is_available() and not no_cuda else "cpu"
+            )
             self.n_gpu = torch.cuda.device_count() if not no_cuda else 0
         else:  # Initializes the distributed backend which will take care of sychronizing nodes/GPUs
             torch.cuda.set_device(self.local_rank)
@@ -143,7 +169,7 @@ class Trainer:
             torch.distributed.init_process_group(backend="nccl")
             self.n_gpu = 1
 
-    def prepare_optimizer(self, model, weight_decay, learning_rate, adam_epsilon):
+    def _prepare_optimizer(self, model, weight_decay, learning_rate, adam_epsilon):
         no_decay = ["bias", "LayerNorm.weight"]
         optimizer_grouped_parameters = [
             {
@@ -165,7 +191,7 @@ class Trainer:
         ]
         return AdamW(optimizer_grouped_parameters, lr=learning_rate, eps=adam_epsilon)
 
-    def prepare_scheduler(optimizer, warmup_ratio, t_total):
+    def _prepare_scheduler(self, optimizer, warmup_ratio, t_total):
         return get_linear_schedule_with_warmup(
             optimizer, num_warmup_steps=int(warmup_ratio * t_total), num_training_steps=t_total
         )
@@ -206,25 +232,19 @@ class Trainer:
             for key, tensor in tensors.items()
         }
 
-    def batch_to_device(self, batch_list):
+    def _batch_to_device(self, batch_list):
         batch_list = tuple(batch.to(self.device) for batch in batch_list)
         return batch_list
 
     def train_epoch(self):
         print(f"Starting epoch {self.logging_epoch}")
         self.model.train()
-        batch_losses = []
-        score_batches = []
-        target_batches = []
 
         epoch_iterator = tqdm(
-            self.train_dataloader, desc="Iteration", disable=self.local_rank not in [-1, 0]
+            self.train_dataloader, desc="Batch", disable=self.local_rank not in [-1, 0]
         )
-        for step, batch in enumerate(
-            epoch_iterator
-        ):  # tqdm(self.train_dl, total=len(self.train_dl))
-
-            batch_list = self.batch_to_device(batch)
+        for step, batch in enumerate(epoch_iterator):
+            batch_list = self._batch_to_device(batch)
             inputs = {
                 "input_ids": batch_list[0],
                 "attention_mask": batch_list[1],
@@ -234,10 +254,10 @@ class Trainer:
                 "labels": batch_list[5],
             }
             outputs = self.model(**inputs)
-            loss = outputs[0]  # model outputs are always tuple in transformers (see doc)
+            loss = outputs[0]  # ? model outputs are always tuple in transformers (see doc)
 
             if self.n_gpu > 1:
-                loss = loss.mean()  # to average on multi-gpu parallel (not distributed) training
+                loss = loss.mean()  # ? to average on multi-gpu parallel (not distributed) training
             if self.gradient_accumulation_steps > 1:
                 loss = loss / self.gradient_accumulation_steps
 
@@ -247,7 +267,8 @@ class Trainer:
             else:
                 loss.backward()
 
-            tr_loss += loss.item()
+            self.tr_loss += loss.item()  # ? Sum up new loss value
+            # ? Optimize (update weights)
             if (step + 1) % self.gradient_accumulation_steps == 0:
                 if self.fp16:
                     torch.nn.utils.clip_grad_norm_(
@@ -265,8 +286,7 @@ class Trainer:
                     self.local_rank in [-1, 0]
                     and self.logging_steps > 0
                     and self.global_step % self.logging_steps == 0
-                ):
-                    # Log metrics
+                ):  # ? Log metrics only once, not in the first step and at every global step
                     if self.local_rank == -1 and self.evaluate_during_training:
                         raise ValueError("Shouldn't `evaluate_during_training` when ft SWDE!!")
 
@@ -274,31 +294,11 @@ class Trainer:
                 epoch_iterator.close()
                 break
 
-
-            self.logger.debug(f"Batch loss: {loss}")
-            batch_losses.append(loss.item())
-            loss.backward()
-
-            nn.utils.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
-
-            self.optimizer.step()
-
-            if self.scheduler:
-                self.scheduler.step()
-
-
-            score_batches.append(similarities_normalized.detach().cpu().numpy())
-            target_batches.append(targets.detach().cpu().numpy())
-
-        all_scores = np.concatenate(score_batches)
-        all_targets = np.concatenate(target_batches)
-        metrics = None
-
         # if wandb.run:
-            # wandb.log(
-            #     {"scores": wandb.Histogram(all_scores)},
-            #     step=self.logging_epoch,
-            # )
+        # wandb.log(
+        #     {"scores": wandb.Histogram(all_scores)},
+        #     step=self.logging_epoch,
+        # )
         # self._log_metrics(
         #     {
         #         "loss": np.mean(batch_losses),
@@ -308,32 +308,57 @@ class Trainer:
         #     prefix="train",
         # )
 
-    def calculate_metrics(self, scores: np.array, targets: np.array) -> Dict[str, float]:
-        pass
-
     def evaluate(self, data_loader: DataLoader) -> Dict[str, float]:
+        print(f"***** Running evaluation *****")
+        # print(f"  Num examples for {website} = {len(data_loader.dataset)}")
+        print(f"  Num examples for dataset = {len(data_loader.dataset)}")
+        print(f"  Batch size = {self.eval_batch_size}")
         self.model.eval()
-        proba_batches = []
-        target_batches = []
-        losses = []
+
+        all_logits = []
+        tr_loss, logging_loss = 0.0, 0.0
         with torch.no_grad():
-            for batch in tqdm(data_loader):
-                batch = self._to_device(batch)
-                targets = batch.pop("labels")
-                similarities_normalized = self.model(batch)
-                probas = self.model.head.calculate_predictions(similarities_normalized)
-                proba_batches.append(probas.cpu().numpy())
-                target_batches.append(targets.cpu().numpy())
-                loss = self.model.head.calculate_loss(similarities_normalized, targets)
-                losses.append(loss.item())
-        all_probas = np.concatenate(proba_batches)
-        all_targets = np.concatenate(target_batches)
-        metrics = None
-        metrics["loss"] = np.mean(losses)
-        return metrics
+            epoch_iterator = tqdm(data_loader, desc="Batch", disable=self.local_rank not in [-1, 0])
+            for step, batch in enumerate(epoch_iterator):
+                batch_list = self._batch_to_device(batch)
+                inputs = {
+                    "input_ids": batch_list[0],
+                    "attention_mask": batch_list[1],
+                    "token_type_ids": batch_list[2],
+                    "xpath_tags_seq": batch_list[3],
+                    "xpath_subs_seq": batch_list[4],
+                    "labels": batch_list[5],
+                }
+                outputs = self.model(**inputs)
+                loss = outputs[0]  # model outputs are always tuple in transformers (see doc)
+
+                if self.n_gpu > 1:
+                    loss = (
+                        loss.mean()
+                    )  # to average on multi-gpu parallel (not distributed) training
+                if self.gradient_accumulation_steps > 1:
+                    loss = loss / self.gradient_accumulation_steps
+
+                if self.fp16:  # TODO (Aimore): Replace this with Accelerate
+                    with amp.scale_loss(loss, self.optimizer) as scaled_loss:
+                        scaled_loss.backward()
+                else:
+                    loss.backward()
+
+                tr_loss += loss.item()
+                if (step + 1) % self.gradient_accumulation_steps == 0:
+                    if self.fp16:
+                        torch.nn.utils.clip_grad_norm_(
+                            amp.master_params(self.optimizer), self.max_grad_norm
+                        )
+                    else:
+                        torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.max_grad_norm)
+
+        #! Compute metrics and return them
+        # return metrics
 
     def train(self):
-        # develop_metrics = self.evaluate(self.develop_dl)
+        # develop_metrics = self.evaluate(self.develop_dataloader)
         # self._log_metrics(develop_metrics, "develop")
 
         print("***** Running training *****")
@@ -365,18 +390,19 @@ class Trainer:
 
             self.logging_epoch = epoch
             self.train_epoch()
+            print(f"Epoch: {epoch} - Loss: {self.tr_loss / self.global_step}")
 
-            if self.global_step > self.max_steps: #! I have changed this
+            if self.global_step > self.max_steps:  #! I have changed this
                 train_iterator.close()
                 break
-            # develop_metrics = self.evaluate(self.develop_dl)
+
+            #! Add evaluation here
+            # develop_metrics = self.evaluate(self.train_dataloader)
             # self._log_metrics(develop_metrics, prefix="develop")
 
         #!Add  # Save the trained model and the tokenizer
-        if self.do_train and (self.local_rank == -1 or torch.distributed.get_rank() == 0):
-            self.save_model_and_tokenizer()
+        # if (self.local_rank == -1 or torch.distributed.get_rank() == 0):
+        #     self.save_model_and_tokenizer()
 
         print("Done training")
         self.model.eval()
-        # store pre-computed label embeddings for fast inference
-        self.model.label_embeddings = self.model.encode_labels()
